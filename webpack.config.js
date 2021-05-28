21fa45926ef1320f343a3db29ad9c6fd54a86602
Nicci Kaufman <niccikaufman@gmail.com>
2021-05-28 12:28:47 -0700

    add dotenv-webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
 module.exports = {
   entry: './src/main.js',
   output: {
@@ -17,7 +19,8 @@ module.exports = {
       title: 'Weather App',
       template: './src/index.html',
       inject: 'body'
-    })
+    }),
+    new Dotenv(),
   ],
   module: {
     rules: [
